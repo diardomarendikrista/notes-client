c70638d892f5a70cbc3cac70164adda3a17aaf72
Diardo Marendi Krista <diardomarendikrista@gmail.com>
2023-05-29 01:03:09 +0700

    add first load for login page, so if already login will only show loading instead of login form

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,16 @@
 import "./styles.css";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
 import Login from "pages/Login";
 import Register from "pages/Register";
+import Loading from "components/Loader/Loader";
 
 export default function Home() {
   const formType = useSelector((state) => state.user.formType);
+  const [firstLoad, setFirstLoad] = useState(true);
 
   const location = useLocation();
 
@@ -18,6 +20,8 @@ export default function Home() {
     // check local storage
     if (localStorage.getItem("access_token")) {
       window.location.replace("/notes");
+    } else {
+      setFirstLoad(false);
     }
   }, []);
 
@@ -29,41 +33,45 @@ export default function Home() {
         <link rel="Note app" href="" />
       </Helmet>
 
-      <div className="home">
-        <Row>
-          <Col lg>
-            <div className="d-flex align-items-center mb-2">
-              <div
-                className="me-1"
-                style={{
-                  borderRadius: "10px",
-                  overflow: "hidden",
-                  width: "55px",
-                }}
-              >
-                <img
-                  src="https://i.imgur.com/wS1NqtG.jpg"
-                  alt="petek"
-                  width="100%"
-                />
+      {firstLoad ? (
+        <Loading center />
+      ) : (
+        <div className="home">
+          <Row>
+            <Col lg>
+              <div className="d-flex align-items-center mb-2">
+                <div
+                  className="me-1"
+                  style={{
+                    borderRadius: "10px",
+                    overflow: "hidden",
+                    width: "55px",
+                  }}
+                >
+                  <img
+                    src="https://i.imgur.com/wS1NqtG.jpg"
+                    alt="petek"
+                    width="100%"
+                  />
+                </div>
+                <h1>Petek Note app</h1>
               </div>
-              <h1>Petek Note app</h1>
-            </div>
-            <p>
-              Register with easy-peasy way to get into our services. Our apps
-              already encrypted with safe way and will not leak to another
-              party. (yeah this is using JWT and Bcrypt)
-            </p>
-            <p>
-              You can create, edit and delete your Note (okay, this is another
-              fc*in CRUD app). honestly this is my Note version that I remake
-              from my past project using Native PHP
-            </p>
-            <p>Hope you enjoy your day with Simple Note!!</p>
-          </Col>
-          <Col lg>{formType === "login" ? <Login /> : <Register />}</Col>
-        </Row>
-      </div>
+              <p>
+                Register with easy-peasy way to get into our services. Our apps
+                already encrypted with safe way and will not leak to another
+                party. (yeah this is using JWT and Bcrypt)
+              </p>
+              <p>
+                You can create, edit and delete your Note (okay, this is another
+                fc*in CRUD app). honestly this is my Note version that I remake
+                from my past project using Native PHP
+              </p>
+              <p>Hope you enjoy your day with Simple Note!!</p>
+            </Col>
+            <Col lg>{formType === "login" ? <Login /> : <Register />}</Col>
+          </Row>
+        </div>
+      )}
     </Container>
   );
 }
